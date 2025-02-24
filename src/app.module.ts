8640c61fe61fae2796908693b9aea0dc63c54113
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2025-02-23 19:26:39 -0500

    Agregar módulo de autenticación con JWT, validación de usuarios y gestión de roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { LoggerConfiguredModule } from './lib/Logger';
 import { PrismaService } from './core/prisma.service';
 import { ResponseInterceptor } from './lib/ResponseInterceptor';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [LoggerConfiguredModule],
+  imports: [LoggerConfiguredModule, AuthModule],
   controllers: [AppController],
   providers: [PrismaService, ResponseInterceptor],
 })
