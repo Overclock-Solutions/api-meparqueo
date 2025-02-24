195c12abf4238881126ad358648f8f187cb33fc9
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2025-02-23 20:50:21 -0500

    Agregar configuración de entorno con ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import configLoader from './lib/ConfigLoader';
 import { LoggerConfiguredModule } from './lib/Logger';
 import { PrismaService } from './core/prisma.service';
 import { ResponseInterceptor } from './lib/ResponseInterceptor';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [LoggerConfiguredModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configLoader],
+      isGlobal: true,
+    }),
+    LoggerConfiguredModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [PrismaService, ResponseInterceptor],
 })
