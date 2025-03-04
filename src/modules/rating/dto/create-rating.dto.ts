12dcc2748ca150c532a214492c60f8a9c72c5957
Isackd <isack2910@gmail.com>
2025-03-03 22:58:57 -0500

    create-rating.dto documentation

--- src/modules/rating/dto/create-rating.dto.ts
+++ src/modules/rating/dto/create-rating.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsNumber,
@@ -8,20 +9,37 @@ import {
 } from 'class-validator';
 
 export class CreateRatingDto {
+  @ApiProperty({
+    description: 'Calificación del 1 al 5',
+    example: '4',
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(1)
   @Max(5)
   rating: number;
 
+  @ApiProperty({
+    description: 'Comentario opcional de la calificación',
+    example: 'Excelente servicio',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   comment?: string;
 
+  @ApiProperty({
+    description: 'Id del usuario que realiza la calificación',
+    example: 'aa222aa4-b001-47fb-ad09-07b2e20bbca1',
+  })
   @IsString()
   @IsNotEmpty()
   userId: string;
 
+  @ApiProperty({
+    description: 'Id del parqueadero a la que se realiza la calificación',
+    example: 'aa222aa4-b001-47fb-ad09-07b2e20bbca1',
+  })
   @IsNotEmpty()
   @IsString()
   parkingLotId: string;
