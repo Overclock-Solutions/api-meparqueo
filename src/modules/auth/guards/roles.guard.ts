8be498b823b3b082c7d2268a06dd11aaf4e797bc
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2025-03-23 21:54:10 -0500

    feat: actualiza el guard de roles para incluir la verificación del estado global del usuario

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Role } from '@prisma/client';
+import { Role, GlobalStatus } from '@prisma/client';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -17,6 +17,10 @@ export class RolesGuard implements CanActivate {
     }
 
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.includes(user.role);
+
+    return (
+      requiredRoles.includes(user.role) &&
+      user.globalStatus === GlobalStatus.ACTIVE
+    );
   }
 }
